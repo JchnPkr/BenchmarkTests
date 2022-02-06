5c9341426feee938908edbaaae5ae736b454be8c
Jochen Peuker <jochen.peuker@binaris-informatik.de>
2022-02-06 13:12:49 +0100

    add and refine tests and log messages

--- src/main/java/de/benchmark/util/Benchmark.java
+++ src/main/java/de/benchmark/util/Benchmark.java
@@ -12,8 +12,11 @@ public class Benchmark {
 	private static long runningTime = 0L;
 
 	/**
-	 * Logs the runningtime of the given predicates and returns their names
-	 * afterwards.
+	 * Logs the running time of the given predicates and returns their names
+	 * afterwards. An error occurring during a test won't interfere with other tests
+	 * and only be logged, although the failed test will not be in the returned list
+	 * of executed tests. This method is intended for simple duration comparison of
+	 * different algorithm implementations.
 	 * 
 	 * @param <T>       type to test
 	 * @param pres      list of predicates to test
@@ -22,40 +25,58 @@ public class Benchmark {
 	 */
 	public static <T> List<String> benchmark(List<BenchmarkPredicate<T>> pres, T testParam) {
 		List<String> benched = new ArrayList<>();
-		long startTime = System.currentTimeMillis();
 
 		pres.forEach(pre -> {
-			LOG.info("type: {}, result: {}, took: {} ms",
-					pre.getName(),
-					pre.getPredicate().test(testParam),
-					(System.currentTimeMillis() - startTime));
+			try {
+				long startTime = System.currentTimeMillis();
+				boolean result = pre.getPredicate().test(testParam);
+				long duration = System.currentTimeMillis() - startTime;
 
-			benched.add(pre.getName());
+				LOG.info("measuring performance of: {}, result: {}, took: {} ms",
+						pre.getName(),
+						result,
+						duration);
+
+				benched.add(pre.getName());
+			} catch (Exception ex) {
+				LOG.error(ex.getMessage());
+			}
 		});
 
 		return benched;
 	}
 
 	/**
-	 * Calculates the runningtime of a function.
+	 * Calculates the running time of a function. This method is intended for
+	 * measuring the duration of a given function where the actual return value
+	 * should be obtained too. The duration time of execution is available by
+	 * calling Benchmark.getRunningTime(). It is available till the next call of
+	 * this method.
 	 *
-	 * @param <T>   Parametertype of the measured function
-	 * @param <R>   Returntype of the measured function
-	 * @param fn    Function the runningtime gets measured for
+	 * @param <T>   Parameter type of the measured function
+	 * @param <R>   Return type of the measured function
+	 * @param fn    Function the running time gets measured for
 	 * @param param Parameter of the function
 	 * @return Return value if the measured function
 	 */
 	public static <T, R> R measurePerformance(String title, Function<T, R> fn, T param) {
 		reset();
 
-		LOG.traceEntry(title);
+		LOG.debug("measuring performance of: {}", title);
 
 		final long startTime = System.currentTimeMillis();
 		R returnValue = fn.apply(param);
 		runningTime = System.currentTimeMillis() - startTime;
 
-		LOG.traceExit("Runningtime: " + runningTime);
+		LOG.debug("Runningtime: {}", runningTime);
 		return returnValue;
+	}
+
+	/**
+	 * resets current running time.
+	 */
+	private static void reset() {
+		runningTime = 0L;
 	}
 
 	/**
@@ -65,12 +86,5 @@ public class Benchmark {
 	 */
 	public static long getRunningTime() {
 		return runningTime;
-	}
-
-	/**
-	 * resets current running time.
-	 */
-	private static void reset() {
-		runningTime = 0L;
 	}
 }
--- src/test/java/de/benchmark/stringcheck/StringEvaluationBenchmarkTest.java
+++ src/test/java/de/benchmark/stringcheck/StringEvaluationBenchmarkTest.java
@@ -1,10 +1,14 @@
 package de.benchmark.stringcheck;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -16,31 +20,45 @@ import de.benchmark.util.BenchmarkPredicate;
 
 class StringEvaluationBenchmarkTest {
 	@Test
+	void benchmarkFailTest() {
+		Predicate<String> npe = (str) -> {
+			return str.isEmpty() ? false : true;
+		};
+
+		List<BenchmarkPredicate<String>> pres = Arrays.asList(
+				new BenchmarkPredicate<>("npe", npe));
+
+		List<String> benched = Benchmark.benchmark(pres, null);
+
+		assertThat(benched).hasSameElementsAs(Collections.emptyList());
+	}
+
+	@Test
 	void stringIsEmptyBenchmarkTest() {
 		Predicate<String> elvis = (str) -> {
-			return str == null || str.isEmpty() ? false : true;
+			return str == null || str.isEmpty() ? true : false;
 		};
 
 		Predicate<String> simpleOrElseTrue = (str) -> {
 			if (str == null || str.isEmpty()) {
-				return false;
-			}
-			return true;
-		};
-
-		Predicate<String> simpleAndElseFalse = (str) -> {
-			if (str != null && !str.isEmpty()) {
 				return true;
 			}
 			return false;
 		};
 
+		Predicate<String> simpleAndElseFalse = (str) -> {
+			if (str != null && !str.isEmpty()) {
+				return false;
+			}
+			return true;
+		};
+
 		Predicate<String> apacheCommons = (str) -> {
-			return !StringUtils.isEmpty(str);
+			return StringUtils.isEmpty(str);
 		};
 
 		Predicate<String> optional = (str) -> {
-			return !Optional.ofNullable(str).orElse(StringUtils.EMPTY).isEmpty();
+			return Optional.ofNullable(str).orElse(StringUtils.EMPTY).isEmpty();
 		};
 
 		List<BenchmarkPredicate<String>> pres = Arrays.asList(
@@ -57,5 +75,30 @@ class StringEvaluationBenchmarkTest {
 		// performance time
 		assertThat(benched).hasSameElementsAs(
 				pres.stream().map(pre -> pre.getName()).collect(Collectors.toList()));
+	}
+
+	@Test
+	void stringContainsPerformanceTest() {
+		String content = "this is a test string";
+		String contains = "is a";
+
+		String titleStringContains = "String.contains";
+		Function<Object[], Boolean> stringFn = (arr) -> ((String) arr[0]).contains((String) arr[1]);
+
+		String titleApacheStringUtilsContains = "ApacheStringUtils.contains";
+		Function<Object[], Boolean> apacheFn = (arr) -> StringUtils.contains((String) arr[0], (String) arr[1]);
+
+		Boolean resultSimpleString = Benchmark.measurePerformance(titleStringContains, stringFn,
+				new Object[] { content, contains });
+		long stringContainsRunningtime = Benchmark.getRunningTime();
+
+		Boolean resultApacheString = Benchmark.measurePerformance(titleApacheStringUtilsContains, apacheFn,
+				new Object[] { content, contains });
+		long apacheContainsRunningtime = Benchmark.getRunningTime();
+
+		assertAll(
+				() -> assertTrue(resultSimpleString),
+				() -> assertTrue(resultApacheString),
+				() -> assertTrue(stringContainsRunningtime < apacheContainsRunningtime));
 	}
 }
